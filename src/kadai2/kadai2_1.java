ea88de3c8d7c3387323954929fb1fa355024f42e
RyuyaMaeda <ryuya.maeda@esm.co.jp>
2020-04-24 17:11:59 +0900

    章２の練習問題提出

---
+++ src/kadai2/kadai2_1.java
@@ -0,0 +1,12 @@
+package kadai2;
+
+// 演算時の型変換を利用して、画面に「x＋yは15」と表示させるプログラム
+
+public class kadai2_1 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+        String ans = "x+yは" + (x + y);
+        System.out.println(ans);
+    }
+}
