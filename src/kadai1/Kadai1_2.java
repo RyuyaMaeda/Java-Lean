3c8ec6fbc410e8e580b80c4ee5caa4adf314d446
RyuyaMaeda <ryuya.maeda@esm.co.jp>
2020-04-27 11:16:01 +0900

    課題１の修正

--- src/kadai1/Kadai1_2.java
+++ src/kadai1/Kadai1_2.java
@@ -1,8 +1,9 @@
 package kadai1;
 
-//画面に”縦幅３横幅５の長方形の面積は、１５”と表示するソースコードの作成
-
-public class kadai1_2 {
+/**
+ * 画面に”縦幅３横幅５の長方形の面積は、１５”と表示するソースコードの作成
+ */
+public class Kadai1_2 {
     public static void main(String[] args) {
         int a = 3;
         int b = 5;
--- src/kadai1/Kadai1_3.java
+++ src/kadai1/Kadai1_3.java
@@ -1,13 +1,14 @@
 package kadai1;
 
-/*以下に示した５つの値を格納するために適した型を考え、初期化する
-* true
-* '駆'
-* 3.14
-* 314159265853979L
-* "ミナトに攻撃！敵に１５ポイントのダメージを与えた。"
+/**
+ * 以下に示した５つの値を格納するために適した型を考え、初期化する
+ * true
+ * '駆'
+ * 3.14
+ * 314159265853979L
+ * "ミナトに攻撃！敵に１５ポイントのダメージを与えた。"
  */
-public class kadai1_3 {
+public class Kadai1_3 {
     public static void main(String[] args) {
         boolean result = true;
         char favoriteCharacter = '駆';
