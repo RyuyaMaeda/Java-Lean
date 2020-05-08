f5cc66126467295a996b448286dfc5dddfff4a4d
RyuyaMaeda <ryuya.maeda@esm.co.jp>
2020-05-08 13:40:57 +0900

    selfAid()の編集

--- src/kadai8/kadai8_4/Cleric.java
+++ src/kadai8/kadai8_4/Cleric.java
@@ -13,10 +13,14 @@ public class Cleric {
     final int MAX_MP = 10;
 
     public void selfAid() {
-        System.out.println(this.name + "はセルフエイドを唱えた！");
-        this.mp -= 5;
-        this.hp = this.MAX_HP;
-        System.out.println("HPが最大まで回復した");
+        if(this.mp >= 5) {
+            System.out.println(this.name + "はセルフエイドを唱えた！");
+            this.mp -= 5;
+            this.hp = this.MAX_HP;
+            System.out.println("HPが最大まで回復した");
+        } else {
+            System.out.println("MPが足りない！");
+        }
     }
 
     public int pray(int second) {
