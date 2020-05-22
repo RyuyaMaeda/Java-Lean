65097049a9f7e41c8a7db8b855f203c5895b2c9a
RyuyaMaeda <ryuya.maeda@esm.co.jp>
2020-05-22 14:51:29 +0900

    練習問題16のプログラムを作成した

---
+++ src/kadai16/kadai16_3/Main.java
@@ -0,0 +1,24 @@
+package kadai16.kadai16_3;
+
+import java.util.*;
+
+/**
+ * 練習問題16-3
+ * 練習16-2と同様に二人の勇者をインスタンス化し、各勇者が倒した敵の数（３、７）と勇者をペアでコレクションに格納
+ * さらに、一つずつ取り出して以下のような画面表示を行う
+ * 「斎藤が倒した敵＝３」
+ * 「鈴木が倒した敵＝７」
+ */
+public class Main {
+    public static void main(String[] args) {
+        Hero hero1 = new Hero("斎藤");
+        Hero hero2 = new Hero("鈴木");
+        Map<Hero, Integer> heroes = new HashMap<Hero, Integer>();
+        heroes.put(hero1, 3);
+        heroes.put(hero2, 7);
+        for (Hero key : heroes.keySet()) {
+            int number = heroes.get(key);
+            System.out.println(key.getName() + "が倒した敵＝" + number);
+        }
+    }
+}
