3ff7726e5048001f6c85ee30f9f621f4cd2c9128
RyuyaMaeda <ryuya.maeda@esm.co.jp>
2020-05-29 10:18:36 +0900

    chore: <>を省略した書き方は推奨されていないため、右辺に<>を追加した

--- src/kadai16/kadai16_3/Main.java
+++ src/kadai16/kadai16_3/Main.java
@@ -17,7 +17,7 @@ public class Main {
     public static void main(String[] args) {
         Hero hero1 = new Hero("斎藤");
         Hero hero2 = new Hero("鈴木");
-        Map<Hero, Integer> heroes = new HashMap();
+        Map<Hero, Integer> heroes = new HashMap<>();
         heroes.put(hero1, 3);
         heroes.put(hero2, 7);
         for (Hero key : heroes.keySet()) {
