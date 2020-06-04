c817061b914c43c02e7d4263c6038f9d6049da8b
RyuyaMaeda <ryuya.maeda@esm.co.jp>
2020-06-04 14:18:08 +0900

    chore: ユーザが硬貨の種類と枚数を指定できるように修正した

--- src/algorithm/kadai4/Main.java
+++ src/algorithm/kadai4/Main.java
@@ -1,5 +1,7 @@
 package algorithm.kadai4;
 
+import java.util.InputMismatchException;
+
 /**
  * 500円、100円、50円、10円、5円、１円が、それぞれ何枚あるかを管理する貯金箱をイメージしたクラス、CoinCaseを作成
  * CoinCaseは指定した硬貨の追加を行うaddCoins()、指定した硬貨の枚数を取得するgetCount()、硬貨の総額を取得するgetAmount()を持つ。
@@ -9,20 +11,22 @@ public class Main {
     public static void main(String[] arg) {
         try {
             CoinCase myCase = new CoinCase();
-            myCase.addCoins(700, 1);
-            myCase.addCoins(100, 4);
-            myCase.addCoins(5, 6);
-            myCase.addCoins(10, 8);
-            myCase.addCoins(50, 3);
-            myCase.addCoins(500, 4);
-            myCase.addCoins(1, 3);
-            myCase.addCoins(50, 3);
-            myCase.addCoins(50, 1);
-            myCase.addCoins(50, 3);
-            System.out.println("指定した硬貨の枚数は" + myCase.getCount(500) + "枚です");
+            for (int i = 0; i < 10; i++) {
+                System.out.println("指定した硬貨の種類は?");
+                int kind = new java.util.Scanner(System.in).nextInt();
+                System.out.println("指定した硬貨の枚数は?");
+                int count = new java.util.Scanner(System.in).nextInt();
+                myCase.addCoins(kind, count);
+            }
+            System.out.println("500円の枚数は" + myCase.getCount(500) + "枚です");
+            System.out.println("100円の枚数は" + myCase.getCount(100) + "枚です");
+            System.out.println("50円の枚数は" + myCase.getCount(50) + "枚です");
+            System.out.println("10円の枚数は" + myCase.getCount(10) + "枚です");
+            System.out.println("5円の枚数は" + myCase.getCount(5) + "枚です");
+            System.out.println("1円の枚数は" + myCase.getCount(1) + "枚です");
             System.out.println("貯金箱に入っているお金の総額は" + myCase.getAmount() + "円です");
-        } catch (NullPointerException e) {
-            System.out.println(e.getMessage());
+        } catch (InputMismatchException e) {
+            System.out.println("不正な値が入力されたため処理を中断しました。値は整数で入力してください");
         }
     }
 }
